5c111f2657a8f202241fa3310145a3ea9ad00e21
Jonatas Pirene <jonatasr.2000@gmail.com>
2022-03-26 00:09:51 -0300

    Started Project

---
+++ lib/tela_login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TelaLogin extends StatefulWidget {
+  const TelaLogin({ Key? key }) : super(key: key);
+
+  @override
+  State<TelaLogin> createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     decoration: new BoxDecoration(color: Color.fromARGB(100, 0, 73, 126)),
+      child: new Center(
+        child: new Text("Open Unifeob")
+        )
+      );
+  }
+}
