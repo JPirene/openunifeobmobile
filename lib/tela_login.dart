02473c8e9a72f2d4c04b32d69083a4e4a1c0446a
Jonatas Pirene <jonatasr.2000@gmail.com>
2022-04-03 21:50:32 -0300

    feature(Open Unifeob Mobile): Criado layout da tela de Login.
    ** Falta melhorar o BottomSheet
    ** Falta Funcionalidades
    ** Resolver Responsividade

--- lib/tela_login.dart
+++ lib/tela_login.dart
@@ -10,11 +10,114 @@ class TelaLogin extends StatefulWidget {
 class _TelaLoginState extends State<TelaLogin> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-     decoration: new BoxDecoration(color: Color.fromARGB(100, 0, 73, 126)),
-      child: new Center(
-        child: new Text("Open Unifeob")
-        )
+    return Scaffold(
+    //  decoration: new BoxDecoration(color: Color.fromARGB(100, 0, 73, 126)),
+      backgroundColor: Color.fromARGB(255, 0,73,126),
+      body: new Container(
+        padding: EdgeInsets.only(top: 200),
+        child: new Column(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[             
+                Text(
+                  "Open Unifeob",
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                ), 
+                SizedBox(
+                  height: 10,
+                  width: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Usuário ou Email', 
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                  width: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Senha', 
+                  ),  
+                ),
+            SizedBox(
+              height: 10,
+              width: 10,
+            ),
+            TextButton(
+              onPressed: null,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.amber),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                minimumSize: MaterialStateProperty.all<Size>(Size(400, 50)), 
+              ),
+              child: new Text("Entrar")
+            ),
+            SizedBox(
+              height: 10,
+              width: 10,
+            ),
+            Text("Esqueceu sua senha? Clique aqui para recuperá-la.",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+              ],
+            ),
+          ],
+        ), 
+        ),
+      bottomSheet: Row(
+        children: <Widget>[
+          Container(
+            height: 150,
+            width: 390,
+            color: Colors.amber,
+            child: Column(
+              children: <Widget>[
+                TextButton(
+                  onPressed: null, 
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 1, 65, 97)), 
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white), 
+                  ),
+                  child: Text("Conectar com Google"),
+                ),
+                TextButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 1, 65, 97)), 
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white), 
+                  ),
+                  child: Text("Conectar com Facebook")
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Criar uma",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white, 
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    TextButton(
+                      onPressed: null, 
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      child: Text("Conta")
+                    )
+                  ],
+                ), 
+              ],
+            ),
+          ), 
+        ],
+      ),
       );
   }
 }
